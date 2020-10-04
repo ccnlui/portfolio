a6d3033fffb3c290e7f9548b2bf33e495d00861e
calvin <ccnlui@gmail.com>
2020-10-04 16:14:29 -0400

    Updates

---
+++ precache-manifest.7adb234bd44131200056bea6dbbd5e57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90172615939bc3d1d4e69422bdaa5477",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5bfa50c35b5e8592623c",
+    "url": "/portfolio/static/css/main.0880625f.chunk.css"
+  },
+  {
+    "revision": "0e2bd5ba1d766635dba5",
+    "url": "/portfolio/static/js/2.887887bd.chunk.js"
+  },
+  {
+    "revision": "5bfa50c35b5e8592623c",
+    "url": "/portfolio/static/js/main.24d01865.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c792d2d7f8031c5eef7fa1812d89dbb0",
+    "url": "/portfolio/static/media/Resume_10.4.2020.c792d2d7.pdf"
+  },
+  {
+    "revision": "00c84e8ac773da70e2b449710a971ca0",
+    "url": "/portfolio/static/media/project-DinoGame.00c84e8a.png"
+  },
+  {
+    "revision": "388c5be44c09bbd3390a3e272adc1a30",
+    "url": "/portfolio/static/media/project-Fotag.388c5be4.png"
+  }
+]);
